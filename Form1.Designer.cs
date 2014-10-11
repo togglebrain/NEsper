37af8d9ef1fe22bc00bf18a5cea456be46ad3994
Meenakshi Kumar <mini.here@gmail.com>
2014-10-11 15:13:05 +0530

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace NesperTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPredict = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtRecd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblPredict
+            // 
+            this.lblPredict.AutoSize = true;
+            this.lblPredict.Location = new System.Drawing.Point(40, 101);
+            this.lblPredict.Name = "lblPredict";
+            this.lblPredict.Size = new System.Drawing.Size(68, 13);
+            this.lblPredict.TabIndex = 1;
+            this.lblPredict.Text = "Processing...";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Minimize",
+            "Maximize",
+            "Lock",
+            "Code",
+            "Coffee",
+            "Blink",
+            "Stare",
+            "Scroll",
+            "Delete"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtRecd
+            // 
+            this.txtRecd.Enabled = false;
+            this.txtRecd.Location = new System.Drawing.Point(43, 41);
+            this.txtRecd.Multiline = true;
+            this.txtRecd.Name = "txtRecd";
+            this.txtRecd.Size = new System.Drawing.Size(181, 57);
+            this.txtRecd.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 162);
+            this.Controls.Add(this.txtRecd);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblPredict);
+            this.Name = "Form1";
+            this.Text = "CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPredict;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtRecd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using com.espertech.esper.client;
+
+namespace NesperTest
+{
+    public delegate void PredictionChangedHandler(string prediction);
+    public partial class Form1 : Form
+    {
+        static EPRuntime _runtime;
+        List<string> eplStatements;
+        MessageHandler mh;
+        Configuration nesperConf;
+        EPServiceProvider epService;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeEPLStatements();
+            InitializeNesper();
+
+            WireEPLStatements();
+
+            txtRecd.Text = "Events: ";
+        }
+
+        private void WireEPLStatements()
+        {
+            foreach (string st in eplStatements)
+            {
+                EPStatement statement = epService.EPAdministrator.CreateEPL(st);
+                statement.Subscriber = mh;
+            }
+        }
+
+        private void InitializeNesper()
+        {
+            nesperConf = new Configuration();
+            nesperConf.AddEventType(typeof(Payload));
+            epService = EPServiceProviderManager.GetDefaultProvider(nesperConf);
+            _runtime = epService.EPRuntime;
+            mh = new MessageHandler();
+            mh.UpdatePrediction += Predict;
+        }
+
+        private void InitializeEPLStatements()
+        {
+            eplStatements = new List<string>();
+            //Minimize->Maximize->Scroll->coffee (recharging)
+            eplStatements.Add("SELECT \"recharging\" FROM pattern [every a=Payload(PayloadText=\"Minimize\") -> every b=Payload(PayloadText=\"Maximize\") -> every c=Payload(PayloadText=\"Scroll\") -> every d=Payload(PayloadText=\"Coffee\")]");
+            //minimize->maximize->scroll (bored)
+            eplStatements.Add("SELECT \"bored\" FROM pattern [every a=Payload(PayloadText=\"Minimize\") -> every b=Payload(PayloadText=\"Maximize\") -> every c=Payload(PayloadText=\"Scroll\")]");
+            //minimize->maximize->lock (gave up)
+            eplStatements.Add("SELECT \"Gave up\" FROM pattern [every a=Payload(PayloadText=\"Minimize\") -> every b=Payload(PayloadText=\"Maximize\") -> every c=Payload(PayloadText=\"Lock\")]");
+            //scroll (on a quest)
+            eplStatements.Add("SELECT \"On a quest\" FROM pattern [every a=Payload(PayloadText=\"Scroll\")]");
+            //blink->blink->stare->delete (angry)
+            eplStatements.Add("SELECT \"Angry\" FROM pattern [every a=Payload(PayloadText=\"Blink\") -> b=Payload(PayloadText=\"Blink\") -> c=Payload(PayloadText=\"Stare\") -> d=Payload(PayloadText=\"Delete\")]");
+            //blink->blink->stare (wondering who wrote that code)
+            eplStatements.Add("SELECT \"Wondering who wrote that code\" FROM pattern [every a=Payload(PayloadText=\"Blink\") -> b=Payload(PayloadText=\"Blink\") -> c=Payload(PayloadText=\"Stare\")]");
+            //blink->blink (In disbelief)
+            eplStatements.Add("SELECT \"In disbelief\" FROM pattern [every a=Payload(PayloadText=\"Blink\") -> b=Payload(PayloadText=\"Blink\")]");
+        }
+
+        public void Predict(string prediction)
+        {
+            lblPredict.Text = "programmer " + prediction;
+        }
+
+        
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _runtime.SendEvent(new Payload() { PayloadText = (sender as ComboBox).SelectedItem.ToString() });
+            txtRecd.Text += "->" + (sender as ComboBox).SelectedItem.ToString();
+        }
+    }
+
+    internal class Payload
+    {
+        public string PayloadText { get; set; }
+    }
+
+    internal class MessageHandler
+    {
+        public event PredictionChangedHandler UpdatePrediction;
+
+        public MessageHandler()
+        {
+        }
+        public void Update(string prediction)
+        {
+            if (UpdatePrediction != null)
+                UpdatePrediction(prediction);
+        }
+    }
+}
